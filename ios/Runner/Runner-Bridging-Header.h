852c6a825c4dbdf1217fb4316c5cb51c8b31522d
Harry Fei <harry_f@enkra.io>
2023-08-30 03:53:49 -0700

    fix ios native symbols bug

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -3,3 +3,11 @@
 #import "../Classes/binding.h"
 
 void *store_dart_post_cobject(void *);
+
+static int64_t dummy_method_to_enforce_bundling(void) {
+  int64_t dummy_var = 0;
+  dummy_var ^= ((int64_t)(void *)store_dart_post_cobject);
+  dummy_var ^= ((int64_t)(void *)calendar_init);
+  dummy_var ^= ((int64_t)(void *)calendar_db_graphql);
+  return dummy_var;
+}
